14adfaee4533f1c6e67f4d9e33efd5f1f9d2aaa6
Nicolas Ambrosis.- <57463307+nicoambrosis@users.noreply.github.com>
2022-05-25 18:36:37 -0300

    Update Index.py

--- Index.py
+++ Index.py
@@ -1,8 +1,3 @@
-<h1 align='center'>
-<img src="https://user-images.githubusercontent.com/57463307/170295762-3319cf01-9efd-44c6-b6ac-c2d6d0194218.png" width="40"/> I'm Nico.- <img src="https://user-images.githubusercontent.com/57463307/170295762-3319cf01-9efd-44c6-b6ac-c2d6d0194218.png" width="40"/>  
-</h1>
-
-
 print('Hello Word')
 print('Added Line')
 
